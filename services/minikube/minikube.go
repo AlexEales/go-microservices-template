e91d9cb7d735c6f3a5af089072f22a5b343f5461
Alex Eales <alexeales@hotmail.co.uk>
2021-07-01 22:24:54 +0100

    Added additional helper functions to minikube lib

--- services/minikube/minikube.go
+++ services/minikube/minikube.go
@@ -9,8 +9,10 @@ import (
 )
 
 var (
+	deleteCmd    = exec.Command("plz", "minikube", "delete")
 	getStatusCmd = exec.Command("plz", "minikube", "status")
 	startCmd     = exec.Command("plz", "minikube", "start")
+	stopCmd      = exec.Command("plz", "minikube", "stop")
 )
 
 // Monitor is a minikube monitor
@@ -20,6 +22,18 @@ type Monitor struct{}
 // used to monior the minikube cluster's health
 func NewMonitor() *Monitor {
 	return &Monitor{}
+}
+
+// Delete attempts to delete the minikube cluster, erroring on fail
+func (m *Monitor) Delete() error {
+	log.Info("Deleting Minikube")
+	cmd := deleteCmd
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	log.Info("Minikube deleted successfully")
+	return nil
 }
 
 // TODO: Eventually should return a status struct which contains fields mapping to plz minikube status output
@@ -47,6 +61,18 @@ func (m *Monitor) IsRunning() (bool, error) {
 	return false, nil
 }
 
+// Remake attempts to delete and re-start the minikube cluster,
+// erroring on fail
+func (m *Monitor) Remake() error {
+	if err := m.Delete(); err != nil {
+		return err
+	}
+	if err := m.Start(); err != nil {
+		return err
+	}
+	return nil
+}
+
 // Start starts the minikube cluster if it is not running already
 // erroring on failure
 func (m *Monitor) Start() error {
@@ -67,5 +93,17 @@ func (m *Monitor) Start() error {
 		log.Info("Minikube already running")
 	}
 
+	return nil
+}
+
+// Stop attempts to stop the minikube cluster, erroring on failure
+func (m *Monitor) Stop() error {
+	log.Info("Stopping Minikube")
+	cmd := stopCmd
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	log.Info("Minikube stopped successfully")
 	return nil
 }
