f1adcae35df6de5d2c01c918ce5e22be866d3417
Alex Eales <alexeales@hotmail.co.uk>
2021-07-11 15:00:17 +0100

    Added connection retrys

--- common/go/db/db.go
+++ common/go/db/db.go
@@ -5,13 +5,29 @@ import (
 	"database/sql"
 	"fmt"
 	"net/url"
+	"time"
 
 	"github.com/jessevdk/go-flags"
 	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
 
+	"go-microservices-template/common/go/backoff"
 	"go-microservices-template/common/go/retry"
 )
+
+var DefaultRetryOpts = &retry.Opts{
+	MaxAttempts: 5,
+	IsRetryable: func(e error) bool {
+		return e != nil
+	},
+	Backoff: &backoff.Exponential{
+		InitialDelay: 2 * time.Second,
+		BaseDelay:    2 * time.Second,
+		MaxDelay:     64 * time.Second,
+		Factor:       2,
+		Jitter:       0,
+	},
+}
 
 // Tx is a wrapper around the sqlx.Tx type to allow consumers of this library to not have to import sqlx
 type Tx = sqlx.Tx
@@ -31,6 +47,8 @@ type DB interface {
 	InTxn(ctx context.Context, fn TxFn) (sql.Result, error)
 	MustExec(query string, args ...interface{}) sql.Result
 	MustExecContext(ctx context.Context, query string, args ...interface{}) sql.Result
+	Ping() error
+	PingContext(ctx context.Context) error
 	Select(dest interface{}, query string, args ...interface{}) error
 	SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
 }
@@ -76,14 +94,20 @@ func NewCommonDB(rawConn *RawConn) *CommonDB {
 
 // DBOpts represents the options that can be provided to connect to a database
 type DBOpts struct {
-	Host     string `short:"h" description:"The address of the host the DB is running on" required:"true" env:"DB_HOST"`
-	Port     uint32 `short:"p" description:"The port the DB is running on" default:"5432" env:"DB_PORT"`
-	User     string `short:"u" description:"The user connection to the database" required:"true" env:"DB_USER"`
-	Password string `short:"p" description:"The password for connection to the database" required:"true" env:"DB_PASSWORD"`
+	Name string `short:"n" description:"The name of the database" required:"true" env:"DB_NAME"`
+	Host string `short:"h" description:"The address of the host the DB is running on" required:"true" env:"DB_HOST"`
+	Port uint32 `short:"p" description:"The port the DB is running on" default:"5432" env:"DB_PORT"`
+	User string `short:"u" description:"The user connection to the database" required:"true" env:"DB_USER"`
+	// TODO: replace with ssl?
+	Password string `long:"password" description:"The password for connection to the database" required:"true" env:"DB_PASSWORD"`
 }
 
 // Validate checks that the opts are populated as required for connecting to a db
 func (o *DBOpts) Validate() error {
+	if o.Name == "" {
+		return fmt.Errorf("DB name cannot be empty string")
+	}
+
 	if _, err := url.Parse(o.Host); err != nil {
 		return err
 	}
@@ -98,7 +122,8 @@ func (o *DBOpts) Validate() error {
 // FormConnectionString takes the opts and forms a database connection string from their values
 func (o *DBOpts) FormConnectionString() string {
 	return fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s sslmode=disable",
+		"database=%s host=%s port=%d user=%s password=%s sslmode=disable",
+		o.Name,
 		o.Host,
 		o.Port,
 		o.User,
@@ -118,34 +143,42 @@ func GetDBOpts() (*DBOpts, error) {
 
 // Conn represents a database connection
 type Conn struct {
-	db          DB
-	isRetryable map[pq.ErrorCode]bool
-	retryOpts   *retry.Opts
+	DB          DB
+	IsRetryable map[pq.ErrorCode]bool
+	RetryOpts   *retry.Opts
 }
 
 // Connect takes database options and attempts to connect to the DB, returning an error on failue
-func Connect(opts *DBOpts) (*Conn, error) {
+func Connect(ctx context.Context, opts *DBOpts) (*Conn, error) {
 	if err := opts.Validate(); err != nil {
 		return nil, err
 	}
 
+	var db *sqlx.DB
+	var err error
 	connStr := opts.FormConnectionString()
-	db, err := sqlx.Connect("postgres", connStr)
+	_, err = retry.Do(ctx, func() error {
+		db, err = sqlx.ConnectContext(ctx, "postgres", connStr)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, DefaultRetryOpts)
 	if err != nil {
 		return nil, err
 	}
 
 	conn := &Conn{
-		db:          NewCommonDB(db),
-		isRetryable: map[pq.ErrorCode]bool{},
-		retryOpts:   retry.DefaultOpts(),
+		DB:          NewCommonDB(db),
+		IsRetryable: map[pq.ErrorCode]bool{},
+		RetryOpts:   DefaultRetryOpts,
 	}
 	return conn, nil
 }
 
 // MustConnect takes database options and attempts to connect to the DB, panicing on error
-func MustConnect(opts *DBOpts) *Conn {
-	conn, err := Connect(opts)
+func MustConnect(ctx context.Context, opts *DBOpts) *Conn {
+	conn, err := Connect(ctx, opts)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module go-microservices-template
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.10.2
+	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
 )
